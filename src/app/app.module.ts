e5ea2c9f8704fc3382347a1f60ea442a241a049b
Ryoma Koike <ryoma.sftrad@gmail.com>
2020-08-30 02:21:15 +0900

    add module and product list and detail components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { AppBoostrapModule } from "./app-bootstrap/app-bootstrap.module";
 import { NavbarComponent } from "./common/navbar/navbar.component";
+import { AppRoutingModule } from ".//app-routing.module";
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent],
-  imports: [BrowserModule, AppBoostrapModule],
+  imports: [BrowserModule, AppBoostrapModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"],
+})
+export class ProductListComponent implements OnInit {
+  products: any = [1, 2, 3, 4];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
